8666b5b338e8fff7ae764d729e80f60c82a59033
Dmytro Zhuk <140021292+dm-zhuk@users.noreply.github.com>
2026-01-15 00:57:35 +0200

    django project setup

---
+++ django-goit/core/settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_db",
+        "USER": "admin",
+        "PASSWORD": "pass",
+        "HOST": "db",
+        "PORT": "5432",
+    }
+}
+
+ALLOWED_HOSTS = ["*"]
